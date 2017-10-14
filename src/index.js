e5a92e586245aef7937fef94fa76bb97b11bd34a
pushandplay <hello@pushandplay.ru>
2017-10-15 01:23:45 +0300

    - refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import {flatten} from '../src/utils';
 import NavigatorDetector from './detectors/NavigatorDetector';
-import HTMLTagDetector from './detectors/HTMLTagDetector';
-import QueryStringDetector from './detectors/QueryStringDetector';
 
 const defaultOptions = {
   fallbackLanguage: 'en',
   queryString: 'lang',
-  detectors: [NavigatorDetector, HTMLTagDetector, QueryStringDetector]
+  detectors: [NavigatorDetector]
 };
 
 class BrowserLanguageDetector {
@@ -17,33 +15,17 @@ class BrowserLanguageDetector {
 
   static detect() {
     if (!this.options) this.config();
-    const detectors = this.getDetectors();
-    const languages = []
-      .concat(
-        detectors.navigator.languages,
-        detectors.queryString.languages,
-        detectors.htmlTag.languages,
-      );
+    const detectors = this.options.detectors.map(A => new A(this.options).detect());
+    this.languages = flatten(detectors.map(a => a.languages));
 
-    this.lang = this.selectPreferredLanguage(languages);
+    this.lang = this.selectPreferredLanguage(this.options.fallbackLanguage, this.languages);
 
     return this;
   }
 
-  static getDetectors() {
-    const detectors = this.options.detectors.map(A => new A(this.options).detect());
-    const langs = detectors.map(a => a.languages);
-    console.log(flatten(langs));
-    return {
-      navigator: new NavigatorDetector(this.options).detect(),
-      queryString: new QueryStringDetector(this.options).detect(),
-      htmlTag: new HTMLTagDetector(this.options).detect(),
-    };
-  }
-
-  static selectPreferredLanguage(languages) {
-    const navigatorLanguageIndex = languages.indexOf(this.options.fallbackLanguage);
-    return languages[navigatorLanguageIndex] || this.options.fallbackLanguage;
+  static selectPreferredLanguage(fallbackLanguage, languages) {
+    const navigatorLanguageIndex = languages.indexOf(fallbackLanguage);
+    return languages[navigatorLanguageIndex] || fallbackLanguage;
   }
 }
 
