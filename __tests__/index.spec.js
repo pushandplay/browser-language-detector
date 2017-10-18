a3942fff9db16080dbf7899445951c47c0410ec9
pushandplay <hello@pushandplay.ru>
2017-10-19 00:10:25 +0300

    ISSUE-3
    - refactoring
    - in progress...

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -17,6 +17,26 @@ describe('BrowserLanguageDetector', () => {
   test('Language should be equal "es" (detect)', () => {
     expect(BrowserLanguageDetector.detect().language).toBe('es');
   });
+
+  test('Languages should be equal ["en", "es"]', () => {
+    expect(BrowserLanguageDetector.applyFallbackLanguage(['en', 'es'], 'en')).toEqual(['en', 'es']);
+  });
+
+  test('Languages should be equal ["ru", "es"]', () => {
+    expect(BrowserLanguageDetector.applyFallbackLanguage(['en', 'es'], 'ru')).toEqual(['ru']);
+  });
+
+  test('Languages should be equal ["ru"]', () => {
+    expect(BrowserLanguageDetector.applyWhiteListLanguages(['en', 'es', 'ru'], ['ru'])).toEqual(['ru']);
+  });
+
+  test('Languages should be equal []', () => {
+    expect(BrowserLanguageDetector.applyWhiteListLanguages(['en', 'es', 'ru'], ['zh'])).toEqual([]);
+  });
+
+  test('Languages should be equal ["en", "es"]', () => {
+    expect(BrowserLanguageDetector.applyWhiteListLanguages(['en', 'es'])).toEqual(['en', 'es']);
+  });
 });
 
 describe('BrowserLanguageDetector.selectPreferredLanguage()', () => {
@@ -24,11 +44,11 @@ describe('BrowserLanguageDetector.selectPreferredLanguage()', () => {
     expect(BrowserLanguageDetector.selectPreferredLanguage).toThrowError('fallbackLanguage is not defined');
   });
   test('Should be equal "en"', () => {
-    expect(BrowserLanguageDetector.selectPreferredLanguage('en')).toEqual('en');
+    expect(BrowserLanguageDetector.selectPreferredLanguage([], 'en')).toEqual('en');
   });
 });
 
-describe('BrowserLanguageDetector with detectors', () => {
+describe('BrowserLanguageDetector with plugins', () => {
   test('Language should be equal "es"', () => {
     const cong = {
       fallbackLanguage: 'es',
--- src/BrowserLanguageDetector.js
+++ src/BrowserLanguageDetector.js
@@ -3,6 +3,7 @@ import {flatten, uniq} from '../src/utils';
 
 const defaultOptions = {
   fallbackLanguage: 'en',
+  // precision: false, TODO:
   // queryString: 'lang', TODO: move into QueryStringPlugin
   plugins: [],
   whiteListLanguages: []
@@ -23,7 +24,8 @@ class BrowserLanguageDetector {
     if (!this.options) this.config();
     const pluginsResult = this.applyPlugins(this.options.plugins, this.options);
     this.languages = uniq(flatten(pluginsResult.map(a => a.languages)));
-    this.language = this.selectPreferredLanguage(this.options.fallbackLanguage, this.languages);
+    this.languages = this.applyWhiteListLanguages(this.languages, this.options.whiteListLanguages);
+    this.language = this.selectPreferredLanguage(this.languages, this.options.fallbackLanguage);
 
     return this;
   }
@@ -40,15 +42,35 @@ class BrowserLanguageDetector {
 
   /**
    * Apply supported languages
-   * @param {array} languages - list of supported languages
-   * @param {string} fallbackLanguage - Fallback language
+   * @param {array} languages - list of detected languages
+   * @param {string} whiteListLanguages - List of supported  languages
    * @return {array} List of languages
    */
-  static applyWhiteListLanguages(languages, fallbackLanguage) {
+  static applyWhiteListLanguages(languages, whiteListLanguages) {
+    if (whiteListLanguages && whiteListLanguages.length) {
+      return languages.filter(language => whiteListLanguages.indexOf(language) !== -1);
+    }
     return languages;
   }
 
-  static selectPreferredLanguage(fallbackLanguage, languages = []) {
+  /**
+   * Apply fallback language
+   * @param {array} languages - List of supported languages
+   * @param {string} fallbackLanguage - Fallback language
+   * @return {array} List of languages
+   */
+  static applyFallbackLanguage(languages, fallbackLanguage) {
+    if (languages.indexOf(fallbackLanguage) !== -1) {
+      return languages;
+    }
+    return [fallbackLanguage];
+  }
+
+  /**
+   * @param {array} languages - List of supported languages
+   * @param {string} fallbackLanguage
+   */
+  static selectPreferredLanguage(languages = [], fallbackLanguage) {
     if (!fallbackLanguage) {
       throw new Error('fallbackLanguage is not defined');
     }
